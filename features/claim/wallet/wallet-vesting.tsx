c0059b7e8c575be7f90f082250f24cad7d576cc1
Nikita Kozlov <nikita.kozlov@p2p.org>
2023-03-22 12:58:43 +0100

    fix: etherscan link depending on chain

--- features/claim/wallet/wallet-vesting.tsx
+++ features/claim/wallet/wallet-vesting.tsx
@@ -1,13 +1,21 @@
+import { getEtherscanLink } from '@lido-sdk/helpers';
 import { Question, Tooltip } from '@lidofinance/lido-ui';
 import AddressBadge from 'components/addressBadge/addressBadge';
 import { WalletCardBalance } from 'components/walletCard';
 import { FC } from 'react';
+import { useWeb3 } from 'reef-knot';
 
 export type WalletVestingProps = {
   vestingAddress: string;
 };
 
 export const WalletVesting: FC<WalletVestingProps> = ({ vestingAddress }) => {
+  const { chainId } = useWeb3();
+
+  if (chainId == null) {
+    return null;
+  }
+
   return (
     <WalletCardBalance
       title={
@@ -20,7 +28,7 @@ export const WalletVesting: FC<WalletVestingProps> = ({ vestingAddress }) => {
       }
       value={
         <a
-          href={`https://etherscan.io/address/${vestingAddress}`}
+          href={getEtherscanLink(chainId, vestingAddress, 'address')}
           target="_blank"
           rel="noreferrer noopener"
         >
