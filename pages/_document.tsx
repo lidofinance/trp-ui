293e409ca3ce9f8fcff7618cd41d4544dc7dd749
Nikita Kozlov <nikita.kozlov@p2p.org>
2023-05-23 13:40:30 +0200

    fix: removed image preview, there is none

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,15 +10,10 @@ import { Fonts, LidoUIHead } from '@lidofinance/lido-ui';
 import { ServerStyleSheet } from 'styled-components';
 import { dynamics } from '../config';
 
-// for prod and dev use https and real domain
-let host = 'http://localhost:3000';
-
 const metaTitle = 'TRP UI | Lido';
 const metaDescription = 'Lido Token Rewards Plan for the contributors.';
 
 const CustomDocument = () => {
-  const metaPreviewImgUrl = `${host}/lido-preview.png`;
-
   return (
     <Html lang="en">
       <Head>
@@ -56,12 +51,10 @@ const CustomDocument = () => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={metaTitle} />
         <meta property="og:description" content={metaDescription} />
-        <meta property="og:image" content={metaPreviewImgUrl} />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metaTitle} />
         <meta name="twitter:description" content={metaDescription} />
-        <meta name="twitter:image:src" content={metaPreviewImgUrl} />
         <meta name="twitter:site" content="@lidofinance" />
         <meta name="twitter:creator" content="@lidofinance" />
 
@@ -82,10 +75,6 @@ CustomDocument.getInitialProps = async (
 ): Promise<DocumentInitialProps> => {
   const sheet = new ServerStyleSheet();
   const originalRenderPage = ctx.renderPage;
-
-  if (ctx?.req?.headers?.host) {
-    host = `https://${ctx?.req?.headers?.host}`;
-  }
 
   try {
     ctx.renderPage = () =>
