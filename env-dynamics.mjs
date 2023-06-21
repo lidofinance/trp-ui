606955c9a02977cb8854c5f03bae70163d439bc9
Nikita <nikita.k@lido.fi>
2023-06-21 18:19:55 +0200

    feat: moved walletconnectProjectId to dynamics

--- env-dynamics.mjs
+++ env-dynamics.mjs
@@ -4,3 +4,6 @@ export const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10) || 1;
 export const supportedChains = process.env?.SUPPORTED_CHAINS?.split(',').map(
   (chainId) => parseInt(chainId, 10),
 ) ?? [1, 4, 5];
+
+/** @type string **/
+export const walletconnectProjectId = process.env.WALLETCONNECT_PROJECT_ID
