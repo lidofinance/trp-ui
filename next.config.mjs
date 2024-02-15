11f75cc1e7da56296bfc25974b2edbfa0437df87
Alexander Belokon <ikatamarinaki@gmail.com>
2024-02-15 13:34:47 +0100

    refactor: remove infura and alchemy keys from config (#98)

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,13 @@ import buildDynamics from './scripts/build-dynamics.mjs';
 
 buildDynamics();
 
-const infuraApiKey = process.env.INFURA_API_KEY;
-const alchemyApiKey = process.env.ALCHEMY_API_KEY;
 const rpcUrls_1 =
   (process.env.EL_RPC_URLS_1 && process.env.EL_RPC_URLS_1.split(',')) ||
-  [
-    alchemyApiKey && `https://eth-mainnet.alchemyapi.io/v2/${alchemyApiKey}`,
-    infuraApiKey && `https://mainnet.infura.io/v3/${infuraApiKey}`,
-  ].filter(Boolean);
+  [].filter(Boolean);
 
 const rpcUrls_5 =
   (process.env.EL_RPC_URLS_5 && process.env.EL_RPC_URLS_5.split(',')) ||
-  [
-    alchemyApiKey && `https://eth-goerli.alchemyapi.io/v2/${alchemyApiKey}`,
-    infuraApiKey && `https://goerli.infura.io/v3/${infuraApiKey}`,
-  ].filter(Boolean);
+  [].filter(Boolean);
 
 const cspTrustedHosts = process.env.CSP_TRUSTED_HOSTS?.split(',') ?? [
   'https://*.lido.fi',
@@ -166,8 +158,6 @@ export default {
     ];
   },
   serverRuntimeConfig: {
-    infuraApiKey,
-    alchemyApiKey,
     rpcUrls_1,
     rpcUrls_5,
     rateLimit,
