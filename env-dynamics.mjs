1892fefad4dc21be05067467865079c3e356f39c
Alexander Belokon <ikatamarinaki@gmail.com>
2024-02-19 12:41:01 +0100

    refactor: update RPC env config (#101)
    * refactor: remove infura and alchemy keys from config
    
    * refactor: add EL_RPC_URLS env variable, replace DEFAULT_CHAIN with CHAIN_ID

--- env-dynamics.mjs
+++ env-dynamics.mjs
@@ -1,5 +1,5 @@
 /** @type number */
-export const defaultChain = parseInt(process.env.DEFAULT_CHAIN, 10) || 1;
+export const defaultChain = parseInt(process.env.CHAIN_ID, 10) || 1;
 /** @type number[] */
 export const supportedChains = process.env?.SUPPORTED_CHAINS?.split(',').map(
   (chainId) => parseInt(chainId, 10),
