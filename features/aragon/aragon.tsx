0d91e47a9085a79e1e0e7b7620a411d6a6f38ad8
Alexander Belokon <ikatamarinaki@gmail.com>
2024-08-26 16:08:59 +0200

    fix: show delegation switch on mainnet

--- features/aragon/aragon.tsx
+++ features/aragon/aragon.tsx
@@ -6,7 +6,6 @@ import { useWeb3 } from 'reef-knot/web3-react';
 import { AragonVote } from './vote/aragonVote';
 import { AragonDelegate } from './delegate/aragonDelegate';
 import { Switch } from '../../shared/ui/switch';
-import { CHAINS } from '@lido-sdk/constants';
 
 const NAV_ROUTES = [
   { name: 'Vote', path: '/aragon' },
@@ -15,14 +14,12 @@ const NAV_ROUTES = [
 
 export const Aragon: FC<{ tab: string }> = ({ tab }) => {
   const isDelegation = tab === 'delegation';
-  const { active, chainId } = useWeb3();
+  const { active } = useWeb3();
   const { data: vestings, isLoading } = useAccountVestings();
   const walletError = useWalletError();
   return (
     <div>
-      {chainId !== CHAINS.Mainnet && (
-        <Switch checked={isDelegation} routes={NAV_ROUTES} />
-      )}
+      <Switch checked={isDelegation} routes={NAV_ROUTES} />
       {!isDelegation && (
         <AragonVote
           active={active}
