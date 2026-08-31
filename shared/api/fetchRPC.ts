15c4f1aacc72ca87d5e49ab3049c53de5e2d84b2
phonktown <phonktown@lido.fi>
2026-08-31 16:25:14 +0400

    fix(RPC): add User-Agent to RPC requests

--- shared/api/fetchRPC.ts
+++ shared/api/fetchRPC.ts
@@ -1,7 +1,22 @@
-import { trackedFetchRpcFactory } from '@lidofinance/api-rpc';
+import {
+  trackedFetchRpcFactory,
+  type TrackedFetchRPC,
+} from '@lidofinance/api-rpc';
 import { metrics, METRICS_PREFIX } from 'features/metrics';
+import { USER_AGENT } from './fetch-external';
 
-export const fetchRPC = trackedFetchRpcFactory({
+const trackedFetchRPC = trackedFetchRpcFactory({
   registry: metrics.registry,
   prefix: METRICS_PREFIX,
 });
+
+export const fetchRPC: TrackedFetchRPC = (url, init, context) => {
+  const headers = new Headers(init.headers);
+  headers.set('User-Agent', USER_AGENT);
+
+  return trackedFetchRPC(
+    url,
+    { ...init, headers: Object.fromEntries(headers.entries()) },
+    context,
+  );
+};
