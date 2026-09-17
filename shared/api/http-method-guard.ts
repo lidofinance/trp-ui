0882f4ef3ea74e7c16b046b37ecd6ebda31b960e
phonktown <phonktown@lido.fi>
2026-09-17 15:33:02 +0400

    fix(Api): end OPTIONS in the http method guard

--- shared/api/http-method-guard.ts
+++ shared/api/http-method-guard.ts
@@ -5,18 +5,18 @@ import { HttpMethod } from './cors';
 export const httpMethodGuard =
   (methodAllowList: HttpMethod[]): RequestWrapper =>
   async (req, res, next) => {
-    if (
-      !req ||
-      !req.method ||
-      !methodAllowList.includes(req.method as HttpMethod)
-    ) {
-      // allow OPTIONS to pass through but still add the Allow header
+    const isAllowed = methodAllowList.some((method) => method === req.method);
+
+    if (!isAllowed) {
       res.setHeader('Allow', methodAllowList.join(', '));
 
-      if (req.method !== HttpMethod.OPTIONS) {
-        res.status(405);
-        throw new Error(`You can use only: ${methodAllowList.toString()}`);
+      if (req.method === HttpMethod.OPTIONS) {
+        res.status(204).end();
+        return;
       }
+
+      res.status(405);
+      throw new Error(`You can use only: ${methodAllowList.join(', ')}`);
     }
 
     await next?.(req, res, next);
